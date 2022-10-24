56c822e7d0c71c419d69377e4f8c4dc9be816692
Austin Asoluka <stracool9@gmail.com>
2022-10-24 11:07:40 +0100

    add fake theme config

---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+export const darkTheme = {
+  color: "#FFFFFF",
+  backgroudColor: "#000000",
+};
+const defaultTheme = {
+  color: "#000000",
+  backgroudColor: "#FFFFFF",
+};
+
+export default defaultTheme;
